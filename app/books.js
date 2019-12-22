2f77e4d2c1039cf817bc9eaba1764371971d567e
Pete <maestro_pete@inbox.ru>
2019-12-22 23:40:43 +0300

    добавил webpack 2

---
+++ app/books.js
@@ -0,0 +1,73 @@
+// ОТКРЫТИЕ-ЗАКРЫТИЕ МЕНЮ ФИЛЬТРОВ
+let myFilters = document.querySelector('#filters');
+let myFiltersTrigger = document.querySelector('#filters-trigger');
+
+if (myFilters && myFiltersTrigger) {
+  myFiltersTrigger.addEventListener('click', function() {
+    myFilters.classList.toggle('filters--open');
+  });
+}
+
+
+// ОТКРЫТИЕ-ЗАКРЫТИЕ МОДАЛКИ
+let myModal = document.querySelector('#modal-book-view');
+let myModalWindow = document.querySelector('.modal__dialog');
+let myModalClose = document.querySelector('.modal__close');
+
+function openModal(item) {
+  myModal.classList.add('modal--open');
+}
+
+function closeModal() {
+  myModal.classList.remove('modal--open');
+}
+
+if (myModalWindow) {
+  myModalWindow.addEventListener('click', function(ev) {
+    ev.stopPropagation();
+  });
+}
+
+if (myModal && myModalClose) {
+  myModal.addEventListener('click', closeModal );
+  myModalClose.addEventListener('click', closeModal );
+}
+
+// РЕНДЕР КНИГ
+let myCatalog = document.querySelector('.catalog__books-list');
+let myBookTmp = document.querySelector('.tmp-book-card');
+
+
+function renderBookItem(item, index) {
+  let myTmpNode = myBookTmp.content.cloneNode(true);
+  // let
+  // myTmpNode.querySelector()
+  myTmpNode.querySelector('.card__img').src = `/img/books/${item["uri"]}.jpg`;
+  myTmpNode.querySelector('.card__img').alt = item.name;
+  myTmpNode.querySelector('.card__title').textContent = item.name;
+  myTmpNode.querySelector('.cart__item-price').textContent = convRUB(item.price);
+  myTmpNode.addEventListener('click', showBookModal);
+  // myTmpNode.querySelector('.field-num__input').value = item.qty;
+  return myTmpNode;
+}
+
+const catalog = document.querySelector('[data-catalog]');
+catalog.addEventListener('click', e => {
+  const { target } = e;
+  const card = target.closest('.card');
+  const id = card.dataset.id
+
+  console.log(card);
+
+  if (card) {
+    openModal(id);
+  } else if (card) {
+    addCart(id);
+  }
+})
+
+function renderBooks() {
+  books.forEach( (item, index, obj) => {
+    myCatalog.appendChild(renderBookItem(item, index));
+  });
+}
---
+++ app/form-validation.js
@@ -0,0 +1,70 @@
+  function showAlert(message) {
+    console.log(message);
+  }
+
+// эл-ты формы, собранные в массив
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+  e.preventDefault();   
+});
+
+const myFormElem = [
+  {
+    name: 'firstName',
+    obj: document.querySelector('input[name="firstname"]'),
+    regExp: new RegExp(/^\W[а-яА-я0-9\-]+\W$/gim),
+    msg: 'Нужно ввести корректное имя',
+  },
+  {
+    name: 'lastName',
+    obj: document.querySelector('input[name="lastname"]'), // поле фамилии,
+    regExp: new RegExp(/^\W[а-яА-я0-9\-]+\W$/gim),
+    msg: 'Нужно ввести корректную фамилию',
+  },
+  {
+    name: 'phone',
+    obj: document.querySelector('input[name="phone"]'), // поле телефона
+    regExp: new RegExp(/(?:(?<=\s)|(?<=^)|(?<=\b))(\+?\s?(\d)\s?\(?(\d{3})\)?\s?(\d{3})\s?\-?\s?(\d{2})\s?\-?\s?(\d{2}))/gim),
+    msg: 'Нужно ввести корректное имя',
+  },
+  {
+    name: 'email',
+    obj: document.querySelector('input[type="email"]'), // поле email
+    regExp: new RegExp(/\b[\w\-\.$]+@[\w\-\.]+\.[a-z]{1,3}\b/gim),
+    msg: 'Нужно ввести корректное имя',
+  },
+];
+
+let myFieldCheckedStyle = 'field-text--input-checked';
+
+// список regExp выражений для проверки форм
+// const myRegExp = {
+//   firstName: /^\W[а-яА-я0-9\-]+\W$/gim,
+//   lastName: /^\W[а-яА-я0-9\-]+\W$/gim,
+//   phone: /(?:(?<=\s)|(?<=^)|(?<=\b))(\+?\s?(\d)\s?\(?(\d{3})\)?\s?(\d{3})\s?\-?\s?(\d{2})\s?\-?\s?(\d{2}))/gim,
+//   email: /\b[\w\-\.$]+@[\w\-\.]+\.[a-z]{1,3}\b/gim,
+// };
+
+// вешаем обработчики событий на эл-ты формы
+myFormElem.forEach((item, index) => {
+  addEventListener('blur', function() {
+    checkFormField(item.obj, item.regExp, myFieldCheckedStyle, item.msg);
+  });
+  addEventListener('change', function() {
+    checkFormField(item.obj, item.regExp, myFieldCheckedStyle, item.msg);
+  });
+});
+
+
+function checkFormField(obj, regExp, okStyle, msg) { // ф-ция проверки поля ввода формы
+  let myText = obj.value.trim();
+  let myParent = obj.parentElement;
+
+  if (myText.match(regExp)) {
+    myParent.classList.add(okStyle);
+  }
+  else {
+    myParent.classList.remove(okStyle);
+    showAlert(msg)
+  };
+};
